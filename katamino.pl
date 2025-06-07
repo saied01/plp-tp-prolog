8313213e54f2deb2bf5100bc2a66de38b8478392
saied01 <rama.saied11@gmail.com>
2025-06-07 20:53:50 -0300

    ej 1 y 2 hechos

--- katamino.pl
+++ katamino.pl
@@ -1,3 +1,28 @@
 :- use_module(piezas).
 
-% Completar ...
+
+% sublista(+Descartar, +Tomar, +L, -R)?: es verdadero cuando R es la sublista de descartar n elementos de L y luego tomar m elementos.
+sublista(Descartar,Tomar,L,R) :- 
+    length(Pref, Descartar), 
+    length(R, Tomar), 
+    append(Pref,Suf,L), 
+    append(R,_,Suf).
+
+
+
+% fila_vacia(+K, -Fila): generar una sola fila de K variables no instanciadas distintas.
+fila_vacia(0, []).
+fila_vacia(K, [_|R]) :-
+    K > 0,
+    K1 is K - 1,
+    fila_vacia(K1, R).
+
+% tablero(+K, -T): generar matriz de 5 filas y K columnas, vacias (variables no instanciadas).
+tablero(K, T) :- 
+    K > 0,
+    fila_vacia(K, F1),
+    fila_vacia(K, F2),
+    fila_vacia(K, F3),
+    fila_vacia(K, F4),
+    fila_vacia(K, F5),
+    T = [F1, F2, F3, F4, F5].
