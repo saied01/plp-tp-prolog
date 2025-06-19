6f118a858068cb6ad14ad564e1de58b6ce64f869
saied01 <rama.saied11@gmail.com>
2025-06-19 14:55:33 -0300

    lo que faltaba

--- katamino.pl
+++ katamino.pl
@@ -1,7 +1,7 @@
 :- use_module(piezas).
 
 
-% sublista(+Descartar, +Tomar, +L, -R)?: es verdadero cuando R es la sublista de descartar n elementos de L y luego tomar m elementos.
+% sublista(+Descartar, +Tomar, +L, -R): es verdadero cuando R es la sublista de descartar Descartar elementos de L y luego tomar Tomar elementos.
 sublista(Descartar,Tomar,L,R) :- 
     length(Pref, Descartar), 
     length(R, Tomar), 
@@ -16,14 +16,14 @@ sublista(Descartar,Tomar,L,R) :-
 % pero como no hay ningun valor valido, sigue aumentando N infinitamente y se cuelga.
     
 
-% fila_vacia(+K, -Fila): generar una sola fila de K variables no instanciadas distintas.
+% fila_vacia(+K, -Fila): es verdadero cuando Fila es una lista de K variables (no instanciadas).
 fila_vacia(0, []).
 fila_vacia(K, [_|R]) :-
     K > 0,
     K1 is K - 1,
     fila_vacia(K1, R).
 
-% tablero(+K, -T): generar la matriz repitiendo las K columnas 5 veces, vacias (variables no instanciadas).
+% tablero(+K, -T): es verdadero cuando T es una matriz de 5 filas y Cols columnas, todas con variables (no instanciadas).
 tablero(K, T) :- 
     K > 0,
     fila_vacia(K, F1),
@@ -44,21 +44,20 @@ contarColumnas([Mh|Mt],C) :- length(Mh, C), contarColumnas(Mt, C).
 
 
 
-% coordenadas(+T, -IJ) debe ser verdadero para todo par IJ que sea una coordenada de elementos del tablero.
-coordenadas(T, IJ) :-
-    IJ = (I,J),
+% coordenadas(+T, -IJ) es verdadero para todo par (I,J) que sea una coordenada de elementos del tablero.
+coordenadas(T, (I,J)) :-
     tamanio(T, F, C),
     between(1, F, I),
     between(1, C, J).
 
 
-% kPiezas(+K, -PS) debe ser verdadero cuando PS es una lista de longitud K de identificadores de piezas.
+% kPiezas(+K, -PS) es verdadero cuando PS es una lista de longitud K de identificadores de piezas.
 kPiezas(K, PS) :- nombrePiezas(L), elegirK(K, L, PS).
 
 % elegirK(+K, +KL, -PS) elige las piezas desde la lista "nombePiezas". Tiene 3 casos: 
-% Base: si K = 0, la lista que corresponde es la vacia.
-% Elige el K actual: La cabeza de la lista de nombres coincide con la cabeza de la lista de kPiezas, y K disminuye en 1.
-% No elige el K actual: Las cabezas de las listas no coinciden por lo que PS se mantiene y K no disminuye.
+% Base: Para K=0 la unica lista que unifica es la vacia.
+% Elige el K actual: Es verdadero cuando la cabeza de la lista de nombres coincide con la cabeza de la lista de kPiezas. K disminuye en 1.
+% No elige el K actual: Es verdadero cuando las cabezas de las listas no coinciden por lo que PS se mantiene y K no disminuye.
 elegirK(0, _, []).
 elegirK(K, [H | KT], [H | PSt]) :- 
     K > 0, 
@@ -69,11 +68,10 @@ elegirK(K, [_ | KT], PS) :-
     elegirK(K, KT, PS).
 
 
-% seccionTablero(+T, +ALTO, +ANCHO, +IJ, ?ST)
+% seccionTablero(+T, +ALTO, +ANCHO, +IJ, ?ST) es verdadero cuando ST es una seccion de tamaño ALTO*ANCHO del tablero T a partir de la coordenada IJ.
 seccionTablero(T, ALTO, ANCHO, (I, J), ST) :- Iindex is I-1, Jindex is J-1, 
                                               sublista(Iindex, ALTO, T, RecorteALTO), 
                                               recorteDeListas(Jindex, ANCHO, RecorteALTO, ST).
-
 
 recorteDeListas(DESCARTAR, TOMAR, [OriginalHead|[]], [RecortadaHead|[]]) :- 
     sublista(DESCARTAR, TOMAR, OriginalHead, RecortadaHead).
@@ -83,20 +81,29 @@ recorteDeListas(DESCARTAR, TOMAR, [OriginalHead|OriginalTail], [RecortadaHead|Re
     recorteDeListas(DESCARTAR, TOMAR, OriginalTail, RecortadaTail).
 
 
+% ubicarPieza(+Tablero, +Identificador) es verdadero cuando la pieza Identificador puede ubicarse en alguna posición del Tablero T.
 ubicarPieza(T, LETRA) :- pieza(LETRA, PIEZA), 
-                         tamanio(PIEZA, AltoPieza, AnchoPieza), tamanio(T, AltoT, AnchoT),
-                         between(1, AnchoT, J), between(1, AltoT, I),
+                         tamanio(PIEZA, AltoPieza, AnchoPieza), 
+                         tamanio(T, AltoT, AnchoT),
+                         between(1, AnchoT, J), 
+                         between(1, AltoT, I),
                          seccionTablero(T, AltoPieza, AnchoPieza, (I,J), PIEZA).
 
 
 
 % ubicarPiezas(+Tablero, +Poda, +Identificadores) lista todas las posibles opciones de ubicar todas las piezas mencionadas en Identificadores.
+% Es verdadero cuando se logra ubicar todas las piezas en Identificadores, sobre el Tablero T, utilizando la poda Poda. 
+% Si la poda es sinPoda, no se realiza ningún chequeo entre colocaciones. Si se especifica otra estrategia,
+% se aplica despues de ubicar cada pieza para descartar tableros invalidos de forma anticipada.
 ubicarPiezas(_, _, []).
-ubicarPiezas(T, P, [Ihead | Itail]) :- ubicarPieza(T, Ihead), ubicarPiezas(T, P, Itail).
-
-% poda(+Poda, +Tablero) es verdadero si el tablero satisface la poda.
-poda(sinPoda, _).
-
+ubicarPiezas(T, sinPoda, [Ihead | Itail]) :-
+    ubicarPieza(T, Ihead),
+    ubicarPiezas(T, sinPoda, Itail).
+ubicarPiezas(T, P, [Ihead | Itail]) :-
+    P \= sinPoda,
+    ubicarPieza(T, Ihead),
+    poda(P, T),
+    ubicarPiezas(T, P, Itail).
 
 
 % llenarTablero(+Poda, +Columnas, -Tablero) enumera todas las formas distintas de llenar un tablero con la cantidad de columnas (y piezas) indicada.
@@ -106,6 +113,8 @@ llenarTablero(P, Cols, T) :-
     ubicarPiezas(T, P, Piezas).
 
 
+% cantSoluciones(+Poda, +Columnas, -N) es verdadero cuando N es igual a la cantidad total de soluciones posibles
+% que hay para un tablero con dada la cantidad de columnas.
 cantSoluciones(Poda, Columnas, N) :-
     findall(T, llenarTablero(Poda, Columnas, T), TS),
     length(TS, N).
@@ -119,12 +128,22 @@ cantSoluciones(Poda, Columnas, N) :-
 % No probamos con K = 5 ya que sin poda tardaria mucho tiempo.
 
 
+% poda(+Poda, +Tablero) es verdadero si el tablero satisface la poda.
 poda(sinPoda, _).
 poda(podaMod5, T) :- todosGruposLibresModulo5(T).
 todosGruposLibresModulo5(T) :- 
-    findall((I,J), 
-    (coordenadas(T, (I,J)), 
-    libre(T, (I,J))), Lista),
+    posicionesLibres(T, CoordLibres),
     agrupar(CoordLibres, Grupos),
     maplist(length, Grupos, Largos),
-    maplist(modulo5, Largos).
+    maplist(mod5, Largos).
+
+
+posicionesLibres(T, CoordLibres) :- 
+    findall((I, J), (coordenadas(T, (I, J)), esLibre(T, (I, J))),CoordLibres).
+
+esLibre(T, (I,J)) :- 
+    nth1(I, T, Fila),
+    nth1(J, Fila, Elem),
+    var(Elem).
+
+mod5(N) :- N mod 5 =:= 0.
